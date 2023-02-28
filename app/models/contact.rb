39d74e689526d76320b48fee17b5ad18c4811df2
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-02-27 19:53:22 -0500

    added an attr_acessor statement to read methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact
+  attr_accessor :first_name, :last_name, :email, :phone_number
+
   def initialize(options)
     @first_name = options[:first_name]
     @last_name = options[:last_name]
